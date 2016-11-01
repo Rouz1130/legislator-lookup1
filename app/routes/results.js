747fbef3748665c331b6d6916e4d3d777dd31975
rouz majlessi <rmajlessi@gmail.com>
2016-11-01 14:07:31 -0700

    fix syntax errors with api key

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,10 +1,12 @@
+
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
-  model: fucntion(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=[7f9240ce7deb45d795ab7331b4d76214]&zip='+ params.zip;
-      return Ember.$.getJson(url).then(function(responseJson) {
-        return responseJson.results;
-      });
+ model: function(params) {
+   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=6eeb2ca57f9c450fa2d5cc4ae9a3cd87&zip=' + params.zip;
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     return responseJSON.results;
+   });
   }
 });
