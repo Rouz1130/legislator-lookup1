2dc59f080dad18d8344d77d7dba90953492ed6f3
rouz majlessi <rmajlessi@gmail.com>
2016-11-01 12:39:38 -0700

    add component function too send ziplookup to our index

--- app/components/lookup-form.js
+++ app/components/lookup-form.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    zipLookup() {
+      var params = {
+        zip: this.get('zip')
+      };              //zipLookup is the action we declared in our template file
+      this.sendAction('zipLookup', params);
+    }
+  }
 });
+//lookup From component(template) contains action LookUp that collects zip from
+//the form and sends it up through the component to index route handler.
